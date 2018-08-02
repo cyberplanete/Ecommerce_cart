b6117ead9a2f5e7b532e0ef1c51f3ed3e6d96da0
pascal <pascal@pascal-desktop>
2018-08-02 19:33:06 +0200

    .

--- src/main/java/shopping_cart/entity/Compte.java
+++ src/main/java/shopping_cart/entity/Compte.java
@@ -1,25 +1,65 @@
 package shopping_cart.entity;
 
-public class Compte {
+import java.io.Serializable;
 
-	public String getRoleUtilisateur() {
-		// TODO Stub de la méthode généré automatiquement
-		return null;
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class Compte implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7410214139500011331L;
+	
+	public static final String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
+
+    private String nomUtilisateur;
+    private String password;
+    private boolean active;
+    private String roleUtilisateur;
+	
+    
+    @Id
+    @Column(name = "Utilisateur_Nom", length = 20, nullable = false)
+    public String getNomUtilisateur() {
+		return nomUtilisateur;
 	}
-
-	public boolean estActif() {
-		// TODO Stub de la méthode généré automatiquement
-		return false;
+    
+    @Column(name = "Password", length = 20, nullable = false)
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
 	}
-
-	public String getnomUtilisateur() {
-		// TODO Stub de la méthode généré automatiquement
-		return null;
-	}
-
 	public String getPassword() {
-		// TODO Stub de la méthode généré automatiquement
-		return null;
+		return password;
 	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Column(name = "Active", length = 1, nullable = false)
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	@Column(name = "Role_Utilisateur", length = 20, nullable = false)
+	public String getRoleUtilisateur() {
+		return roleUtilisateur;
+	}
+	public void setRoleUtilisateur(String roleUtilisateur) {
+		this.roleUtilisateur = roleUtilisateur;
+	}
+	@Override
+	public String toString() {
+		return "Compte [nomUtilisateur=" + nomUtilisateur + ", password=" + password + ", roleUtilisateur="
+				+ roleUtilisateur + "]";
+	}
+    
+    
+
 
 }
