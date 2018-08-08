6fbbc45a123595beac7713e479b179e78922432c
pascal <pascal@pascal-desktop>
2018-08-08 20:41:44 +0200

    .

--- src/main/java/shopping_cart/model/InformationProduit.java
+++ src/main/java/shopping_cart/model/InformationProduit.java
@@ -1,7 +1,30 @@
 package shopping_cart.model;
 
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import shopping_cart.entity.Produit;
+
 public class InformationProduit {
 
+	private String code;
+	private String nom;
+	private double prix;
+	
+	private boolean newProduit = false;
+	
+	//Téléversement du fichier
+	private CommonsMultipartFile fichierData;
+	
+	
+	public InformationProduit() {
+	}
+	
+	public InformationProduit(Produit produit) {
+		this.code = produit.getCode();
+		this.nom = produit.getNom();
+		this.prix = produit.getPrix();
+	}
+	
 	public int getPrix() {
 		// TODO Stub de la méthode généré automatiquement
 		return 0;
