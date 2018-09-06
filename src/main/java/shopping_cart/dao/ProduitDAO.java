9a8a8b2c93f0b0170b5ab9d4c278a4b2a53d1f63
pascal <pascal@pascal-desktop>
2018-09-06 14:39:47 +0200

    .

--- src/main/java/shopping_cart/dao/ProduitDAO.java
+++ src/main/java/shopping_cart/dao/ProduitDAO.java
@@ -1,5 +1,20 @@
 package shopping_cart.dao;
 
-public class ProduitDAO {
+import shopping_cart.entity.Produit;
+import shopping_cart.model.InformationProduit;
+import shopping_cart.model.PaginationResultat;
 
+public interface ProduitDAO {
+
+	public Produit findProduit(String code);
+	
+	public InformationProduit findInformationProduit(String code);
+	
+	public PaginationResultat<InformationProduit> queryProducts(int page,
+             int maxResult, int maxNavigationPage  );
+
+	public PaginationResultat<InformationProduit> queryProducts(int page, int maxResult,
+             int maxNavigationPage, String likeName);
+
+	public void save(InformationProduit productInfo);
 }
